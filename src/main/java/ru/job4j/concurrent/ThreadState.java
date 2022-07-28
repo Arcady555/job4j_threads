3f92a54549a25a0c0444e4a8b0c5f41c0a969d4b
Arrcady555 <parfenov7233@gmail.com>
2022-07-28 12:53:28 +0300

    Состояние нити. [#229175]-3

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -10,8 +10,15 @@ public class ThreadState {
         );
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
-            System.out.println(Thread.currentThread().getState());
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.print("");
+            /**
+             * Может лучше всё-таки так оставить, вместо строки ниже?
+             * В задании не требовалось распечатывать RUNNABLE...)))
+             *
+             * System.out.println(Thread.currentThread().getState());
+             *
+             */
         }
         System.out.println("Работа завершена");
     }
