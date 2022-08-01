f2bc6a90e0a3526fda7d7d17f63c6ed8b56458e7
Arrcady555 <parfenov7233@gmail.com>
2022-08-02 02:33:16 +0300

    Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/concurrent/UserCache.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        for (int id = 1; id <= users.size(); id++) {
+            list.add(findById(id));
+        }
+        return list;
+    }
+}
