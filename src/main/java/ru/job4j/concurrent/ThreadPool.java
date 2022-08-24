aca545ea99e54302296c544b6ac20caee7bc9102
Arrcady555 <parfenov7233@gmail.com>
2022-08-24 20:23:56 +0300

    Реализовать ThreadPool [#1099]-3

--- src/main/java/ru/job4j/concurrent/ThreadPool.java
+++ src/main/java/ru/job4j/concurrent/ThreadPool.java
@@ -8,23 +8,45 @@ import java.util.List;
 @ThreadSafe
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private int size = Runtime.getRuntime().availableProcessors();
+    private final int size = Runtime.getRuntime().availableProcessors();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
 
-    public ThreadPool() throws InterruptedException {
+    public ThreadPool() {
         for (int i = 0; i < size; i++) {
-            threads.add(new Thread(tasks.poll()));
-            notifyAll();
+            Thread thread = new Thread(
+                    () -> {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                tasks.poll();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+                    );
+            thread.start();
+            threads.add(thread);
         }
     }
 
     public void work(Runnable job) throws InterruptedException {
         tasks.offer(job);
+        job.run();
     }
 
     public void shutdown() {
         for (Thread thread : threads) {
             thread.interrupt();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        for (int i = 1; i <= 10; i++) {
+            int number = i;
+            Runnable runnable = () -> System.out.println("Выполняется нить " + number);
+            threadPool.work(runnable);
+        }
+        threadPool.shutdown();
     }
 }
